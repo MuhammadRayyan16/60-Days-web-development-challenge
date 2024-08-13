fe09cb7f3019aa7a174dfc51d31f5e51c0733064
Rayyan7878 <rayyanshaikh787898@gmail.com>
2024-08-13 00:15:46 -0700

    first commit

---
+++ 30 Days  Javascript Project/Day1 Practice Project/script.js
@@ -0,0 +1,105 @@
+//  var let const
+
+// var name = "rayyan";
+// console.log(name);
+
+// let name = "rayyan";
+// console.log(name);
+
+// const name = "rayyan";
+// console.log(name);
+
+// let numbers = [1, 2, 3, 4, 5];
+// console.log(numbers);
+
+// const colors = ["red", "green", "yellow", "blue", "pink", "orange", "purple"];
+// colors.push("brown");
+
+// console.log(colors);
+
+// const colors = ["red", "green", "yellow", "blue", "pink", "orange", "purple"];
+// colors.pop();
+
+// console.log(colors);
+
+// const colors = ["red", "green", "yellow", "blue", "pink", "orange", "purple"];
+// colors.shift();
+
+// console.log(colors);
+
+// const colors = ["red", "green", "yellow", "blue", "pink", "orange", "purple"];
+// colors.unshift("gray");
+
+// console.log(colors);
+
+// const colors = ["red", "green", "yellow", "blue", "pink", "orange", "purple"];
+// colors.slice(2);
+
+// console.log(colors);
+
+// const colors = ["red", "green", "yellow", "blue", "pink", "orange", "purple"];
+// colors.sort();
+
+// console.log(colors);
+
+// const user = {
+//     name: "Rayyan",
+//     age: 16,
+//     country: "Pakistan",
+//     city: "karachi",
+//     hobbies: ["reading", "coding", "swimming", "performance"]
+// }
+
+// console.log(user);
+
+// function name() {
+//     console.log("rayyan");
+// }
+
+
+// const picuser = () => {
+//     name()
+// }
+
+// picuser()
+
+
+
+
+
+// let user = 12;
+
+// if (user < 14) {
+//     console.log("User is older than 14");
+// } else {
+//     console.log("User is not older than 14");
+// }
+
+
+
+
+
+
+
+// for (let i = 1; i < 11; i++) {
+//     console.log(i);
+// }
+
+
+// for (let i = 25; i < 51; i++) {
+//     console.log(i);
+// }
+
+
+// for (let i = 1; i < 101; i++) {
+//     console.log(i);
+// }
+
+
+
+// let data = 12;
+
+// while (data < 15) {
+//     console.log(data);
+//     data++;
+// }
---
+++ 30 Days  Javascript Project/Day2 Practics Project/script.js
@@ -0,0 +1,44 @@
+// let user = {
+//     name: 'John Doe',
+//     age: 30,
+//     address: '123 Main'
+// }
+
+// delete user.address;
+// console.log(user);
+
+// function create() {
+//     alert("create this Element");
+// }
+
+
+// setTimeout(function () {
+//     create()
+// }, 3000)
+
+
+// let obj = {
+//     name: 'John Doe',
+//     age: 30,
+//     address: '123 Main'
+// }
+//  console.log(obj);
+
+
+// let arr = [1, 23, 4, 5, 6];
+
+// let sum = [...arr]
+// console.log(sum);
+
+
+
+// if (12 > 15) {
+//     console.log("right");
+// } else {
+
+//     console.log("wrong");
+// }
+
+
+
+
