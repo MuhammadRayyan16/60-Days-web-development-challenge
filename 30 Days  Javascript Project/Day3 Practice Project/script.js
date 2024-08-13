a9691c62cc2e4280f8b9d37e859f1b73f79e4861
Rayyan7878 <rayyanshaikh787898@gmail.com>
2024-08-13 02:54:50 -0700

    done

--- 30 Days  Javascript Project/Day3 Practice Project/script.js
+++ 30 Days  Javascript Project/Day3 Practice Project/script.js
@@ -3,7 +3,15 @@ let input = document.getElementById('input').value;
 let list = document.getElementById('list');
 
 
-const handleclick = () => {
-    console.log(input.value);
+const handlebtn = () => {
 
+    list.innerHTML += `<div><h1>${input.value}</h1>
+    <button onclick'this'>Delete</button>
+    </div>`;
+
+    input.value = '';
+}
+
+const handleDelete = (e) => {
+    e.target.parentElement.remove();
 }
