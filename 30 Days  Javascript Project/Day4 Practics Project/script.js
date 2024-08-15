0d9a29521771c734cf6bd930868318a6f5936d72
Rayyan7878 <rayyanshaikh787898@gmail.com>
2024-08-15 04:04:41 -0700

    done

---
+++ 30 Days  Javascript Project/Day4 Practics Project/script.js
@@ -0,0 +1,20 @@
+// let timer = document.querySelector(".timer");
+
+// function gettime() {
+
+//     let now = new Date()
+
+//     hours = now.getHours();
+//     minutes = now.getMinutes();
+//     seconds = now.getSeconds();
+
+//     hours = hours < 10 ? "0" + hours : hours;
+//     minutes = minutes < 10 ? "0" + minutes : minutes;
+//     seconds = seconds < 10 ? "0" + seconds : seconds;
+
+//     let fulltime = hours + ":" + minutes + ":" + seconds;
+
+//     timer.textContent = fulltime;
+
+// }
+// setInterval(gettime, 1000)
