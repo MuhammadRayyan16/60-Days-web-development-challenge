4f5028e0f2ab88187f200397ee3c1db48a5357ac
Rayyan7878 <rayyanshaikh787898@gmail.com>
2024-08-19 06:11:15 -0700

    done

---
+++ 30 Days  Javascript Project/Day7 Practics Project/joke.js
@@ -0,0 +1,24 @@
+const btn = document.querySelector(".joke-button");
+const container = document.querySelector(".joke-text");
+
+const jokeapi = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,political,racist,explicit";
+
+const getjoke = () => {
+    container.classList.remove("fade");
+    fetch(jokeapi)
+        .then(data => data.json())
+        .then(item => {
+            if (item.type === "single") {
+                container.textContent = item.joke;
+            } else if (item.type === "twopart") {
+                container.textContent = `${item.setup} - ${item.delivery}`;
+            }
+            container.classList.add("fade");
+        })
+        .catch(error => {
+            container.textContent = "Oops! Something went wrong. Please try again.";
+        });
+};
+
+btn.addEventListener("click", getjoke);
+getjoke();
